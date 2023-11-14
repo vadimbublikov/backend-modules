5e3e1a2d347f185020c534adba788e502879fdf9
Bublikov Vadim <vadimbublikov@yandex.ru>
2023-11-14 23:12:11 +0300

    app-main

---
+++ demo/app-main/src/main/java/tech.bvd.app/MainClass.java
@@ -0,0 +1,14 @@
+package tech.bvd.app;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainClass {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainClass.class, args);
+    }
+
+}
