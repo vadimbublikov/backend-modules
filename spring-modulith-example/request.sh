fb09b3bc22df25f7956fbdf49638f981889cfdc2
Bublikov Vadim <vadimbublikov@yandex.ru>
2024-09-29 17:18:03 +0300

    up spring modulith version

--- spring-modulith-example/request.sh
+++ spring-modulith-example/request.sh
@@ -2,11 +2,9 @@ curl 'http://localhost:8080/catalog/list' \
   -H 'Content-Type: application/json' \
   --data-raw '{}'
 
-
 #curl 'http://localhost:8080/catalog/product-to-cart' \
 #  -H 'Content-Type: application/json' \
 #  --data-raw '{ "productId": "grill" }'
-
 
 #curl 'http://localhost:8080/catalog/product-to-order' \
 #  -H 'Content-Type: application/json' \
